959062089c2621fa6f0b5ea65be11fa3f261c69c
Benjamin <benjamind@situve.com>
2019-04-30 15:17:50 +0200

    Added & modified functions

--- utility.js
+++ utility.js
@@ -1,10 +1,18 @@
+/**
+ * 2D coordinate
+ */
 class Point {
-    constructor(x, y){
-      this.x = x;
-      this.y = y;
-    }
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
   }
+}
 
+/**
+ * Returns mouse position based on canvas & mouse event
+ * @param {*} canvas 
+ * @param {*} e 
+ */
 function getMousePos(canvas, e) {
     let rect = canvas.getBoundingClientRect();
     let x = e.clientX - rect.left;
@@ -12,6 +20,11 @@ function getMousePos(canvas, e) {
     return new Point(x, y);
 }
 
+/**
+ * Returns true if item is contained in the array
+ * @param {*} array 
+ * @param {*} item 
+ */
 function contains(array, item){
     for(var i = 0; i < array.length; i++){
         if(array[i].equals(item)){
@@ -21,15 +34,28 @@ function contains(array, item){
     return false;
 }
 
+/**
+ * Returns a random color hsl(0-360)
+ */
 function randomColor(){
   return `hsl(${Math.floor(Math.random()*360)}, 100%, 50%)`
 }
 
 /**
- * This can end up in an infinite array if a 
- * child-object references itself
  * @param {Array} array 
  */
 function copyOfArray(array){
-    return JSON.parse(JSON.stringify(array))    
+    return [...array];   
+}
+
+/**
+ * Turns a 2D array into a 1D array
+ * ex: flattenArray([[1,2],[3,4]]) -> [1,2,3,4]
+ * @param {*} array 
+ */
+function flattenArray(array){
+    return array.reduce((acc, item, index) => {
+        const flatten = acc.concat(item);
+        return flatten;
+      });
 }
